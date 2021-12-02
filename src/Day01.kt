5b96c353d1a3b1ac84de8b73b271e55fa82f5620
Michael Hackner <mhackner@gmail.com>
2021-12-01 16:51:32 -0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,14 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.map { it.toInt() }
+        .zipWithNext { a, b -> if (a < b) 1 else 0 }.sum()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>) = input.map { it.toInt() }
+        .windowed(3, transform = { it.sum() })
+        .zipWithNext { a, b -> if (a < b) 1 else 0 }.sum()
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
